4a885bc9066e2f987491509069d2d251d869c2b0
Muhammad Hasan - TEXPO <hasan@texpo.local>
2018-08-18 05:12:07 -0700

    yeah

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import promise from 'redux-promise';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+import RootReducer from './reducers';
+
+const middleware = applyMiddleware(promise, thunk, logger);
+
+const Store = createStore(
+    RootReducer,
+    compose(middleware)
+);
+
+export default Store;
