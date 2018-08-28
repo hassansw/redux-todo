ab251dc9d62d1eb0154b5d409164351bce9c6297
Muhammad Hasan - TEXPO <hasan@texpo.local>
2018-08-28 14:03:53 +0500

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ const Footer = () => (
         <span>Show: </span>
         <FilterLink filter={VisibilityFilters.SHOW_ALL}>
             All
-    </FilterLink>
+        </FilterLink>
         <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>
             Active
-    </FilterLink>
+        </FilterLink>
         <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>
             Completed
-    </FilterLink>
+        </FilterLink>
     </div>
 )
 
--- src/containers/AddTodo.js
+++ src/containers/AddTodo.js
@@ -2,28 +2,28 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../redux/actions'
 
+let input
 const AddTodo = ({ dispatch }) => {
-    let input
 
     return (
         <div>
-            <form
-                onSubmit={e => {
-                    e.preventDefault()
-                    if (!input.value.trim()) {
-                        return
-                    }
-                    dispatch(addTodo(input.value))
-                    input.value = ''
-                }}
-            >
+            <form onSubmit={e => onSubmitAction(e, dispatch)}>
                 <input ref={node => input = node} />
                 <button type="submit">
                     Add Todo
-        </button>
+                </button>
             </form>
         </div>
     )
+}
+
+const onSubmitAction = (e, dispatch) => {
+    e.preventDefault()
+    if (!input.value.trim()) {
+        return
+    }
+    dispatch(addTodo(input.value))
+    input.value = ''
 }
 
 export default connect()(AddTodo)
